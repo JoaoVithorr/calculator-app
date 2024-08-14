9abb1cb499ae4bde9bede0362b1c67c5d0854798
João Vithor <joaovithormg@gmail.com>
2024-08-14 18:00:51 -0300

    Adding calculator functions

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -5,6 +5,11 @@ import Button from "../components/Button";
 import Display from "../components/Display";
 
 export default class Calculator extends Component {
+    constructor(props) {
+        this.clearMemory = this.clearMemory.bind(this)
+        this.setOperation = this.setOperation.bind(this)
+        this.addDigit = this.addDigit.bind(this)
+    }
     clearMemory() {
         console.log('apagar')
     }
@@ -16,25 +21,25 @@ export default class Calculator extends Component {
     addDigit(n) {
         console.log(n)
     }
-    
+
     render() {
         return (
         <div className="calculator">
             <Display value ="100"/>
-            <Button label="AC" />
-            <Button label="/" />
+            <Button label="AC" click={this.clearMemory}/>
+            <Button label="/" click={this.setOperation}/>
             <Button label="7" />
             <Button label="8" />
             <Button label="9" />
-            <Button label="*" />
+            <Button label="*" click={this.setOperation}/>
             <Button label="4" />
             <Button label="5" />
             <Button label="6" />
-            <Button label="-" />
+            <Button label="-" click={this.setOperation}/>
             <Button label="1" />
             <Button label="2" />
             <Button label="3" />
-            <Button label="+" />
+            <Button label="+" click={this.setOperation}/>
             <Button label="0" />
             <Button label="." />
             <Button label="=" />
